2351ff26bc848a5fe0563fdd752df1f0e4cc0f7b
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-14 15:15:13 +0200

    feat: create form with inputs and send data to API with POST method printing response on console

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
+//importo useState
+import { useState } from "react"
+//importo axios
+import axios from "axios";
+
+//salvo in una variabile l'url dell'API fornita
+const urlPosts = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
 
 function App() {
 
+  //creo la variabile con cui andrò a lavorare per gestire i vari input
+  const [formPost, setFormPost] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  })
+
+  //creo una funzione per gestire il valore del campo di input
+  function handleFormPost(event) {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+
+    setFormPost(formPost => ({
+      ...formPost,
+      [event.target.name]: value,
+    }))
+  }
+
+  //creo una funzione per mandare i dati compilati nel form ed effettuare la chiamata all'API con il metodo POST
+  function sendPost(event) {
+    event.preventDefault();
+    console.log('ho mandato i dati per il nuovo post');
+
+    //effettuo la chiamata in POST e inserisco l'url dell'API seguito dalla variabile che conterrà i dati inseriti nei campi di input
+    axios.post(urlPosts, formPost)
+      //mostro come risposta i dati che ho mandato tramite il form
+      .then(response => {
+        console.log("Risposta dall'API", response.data);
+
+        //dopo aver mandato i dati, vado a svuotare i campi
+        setFormPost({
+          author: '',
+          title: '',
+          body: '',
+          public: false
+        })
+      })
+
+  }
 
   return (
     <>
-      Ciao sono in App
+      <form onSubmit={sendPost}>
+        <label htmlFor="author-name">Nome autore</label>
+        <input type="text"
+          id="author-name"
+          name='author'
+          value={formPost.author}
+          onChange={handleFormPost}
+          placeholder="Autore del post" />
+
+        <label htmlFor="title-post">Titolo del post</label>
+        <input type="text"
+          id="title-post"
+          name='title'
+          value={formPost.title}
+          onChange={handleFormPost}
+          placeholder="Titolo del post" />
+
+        <label htmlFor="text-content">Contenuto del post</label>
+        <input type="text"
+          id="text-content"
+          name='body'
+          value={formPost.body}
+          onChange={handleFormPost}
+          placeholder="Testo del post" />
+
+        <label htmlFor="public-post">Pubblico:</label>
+        <input type="checkbox"
+          id="public-post"
+          name='public'
+          checked={formPost.public}
+          onChange={handleFormPost} />
+
+        <button>Crea il post</button>
+      </form>
     </>
   )
 }
